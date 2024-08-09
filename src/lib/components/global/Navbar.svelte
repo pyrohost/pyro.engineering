52395211ac880dfdc7391d79b475f796f9cb7abb
Null Pointer <nullptr@pyro.host>
2024-08-09 16:23:15 +0100

    feat: parallax bg

--- src/lib/components/global/Navbar.svelte
+++ src/lib/components/global/Navbar.svelte
@@ -12,7 +12,7 @@
 		searchOpen = true;
 		setTimeout(() => {
 			searchBtn.querySelector("input")?.focus();
-		}, 20);
+		}, 50);
 	};
 
 	const closeSearch = () => {
@@ -27,65 +27,73 @@
 </script>
 
 <div
-	class="absolute left-1/2 flex h-24 w-full max-w-[1400px] -translate-x-1/2 items-center border border-t-0 border-dashed border-neutral-700 bg-[#0e0e0e] px-8 shadow-xl shadow-black/75"
+	class="fixed left-1/2 z-50 flex h-24 w-full -translate-x-1/2 items-center justify-center border-b border-dashed border-neutral-700 bg-black/65 px-8 shadow-xl shadow-black/75 backdrop-blur-md"
 >
-	<div class="flex flex-grow items-center">
-		<div class="h-full opacity-60">
-			<LogoWordmark />
+	<div class="flex h-full w-full max-w-[1400px] items-center justify-center">
+		<div class="flex flex-grow items-center">
+			<a class="flex cursor-pointer" href="/">
+				<div class="mr-3.5 h-full opacity-60">
+					<LogoWordmark />
+				</div>
+				<div class="hidden items-center sm:flex">
+					<div class="h-8 w-0.5 bg-white/30" />
+					<div class="ml-4 select-none text-xl text-white/50">Engineering</div>
+				</div>
+			</a>
 		</div>
-		<div class="ml-3.5 h-8 w-0.5 bg-white/30" />
-		<div class="ml-4 text-xl text-white/50">Engineering</div>
-	</div>
-	<div class="flex flex-shrink-0 items-center gap-6">
-		<button
-			class={clsx("relative h-12", {
-				"w-72 cursor-default hover:!bg-[#191919] active:!bg-[#191919]": searchOpen,
-				"w-12": !searchOpen,
-			})}
-			style="transition: width 200ms ease-out;"
-			bind:this={searchBtn}
-			on:mousedown={openSearch}
-		>
-			<div
-				class={clsx(
-					"absolute top-0 flex h-full w-full items-center justify-center transition-opacity duration-200 ease-out",
-					{
-						"pointer-events-auto opacity-0": searchOpen,
-						"pointer-events-none opacity-100": !searchOpen,
-					},
-				)}
+		<div class="flex flex-shrink-0 items-center gap-6">
+			<button
+				class={clsx("relative h-12", {
+					"w-72 cursor-default hover:!bg-[#191919] active:!bg-[#191919]": searchOpen,
+					"w-12": !searchOpen,
+				})}
+				style="transition: width 200ms ease-out;"
+				bind:this={searchBtn}
+				on:click={openSearch}
 			>
-				Search
-			</div>
-			<!-- this is a really bad way of ensuring there's no layout shift -->
-			{#if jsRun}
 				<div
 					class={clsx(
 						"absolute top-0 flex h-full w-full items-center justify-center transition-opacity duration-200 ease-out",
 						{
-							"pointer-events-none opacity-0": !searchOpen,
-							"pointer-events-auto opacity-100": searchOpen,
+							"pointer-events-auto opacity-0": searchOpen,
+							"pointer-events-none opacity-100": !searchOpen,
 						},
 					)}
 				>
-					<button
-						class="absolute right-4 h-fit w-fit min-w-0 border-none !bg-transparent p-0"
-						on:click={closeSearch}
-					>
-						<iconify-icon icon="heroicons-solid:search" class="text-white/50" />
-					</button>
-					<input
-						on:blur={closeSearch}
-						class="h-full w-full px-4"
-						type="text"
-						placeholder="Search"
-					/>
+					Search
 				</div>
-			{/if}
-		</button>
-		<a class="btn h-12" href="##">All posts</a>
-	</div>
-	<div class="pointer-events-none absolute left-0 top-0 h-full w-full">
-		<Noise colour="#1a1a1a" />
+				<!-- this is a really bad way of ensuring there's no layout shift -->
+				{#if jsRun}
+					<div
+						class={clsx(
+							"absolute top-0 flex h-full w-full items-center justify-center transition-opacity duration-200 ease-out",
+							{
+								"pointer-events-none opacity-0": !searchOpen,
+								"pointer-events-auto opacity-100": searchOpen,
+							},
+						)}
+					>
+						<button
+							tabindex={searchOpen ? 0 : -1}
+							class="absolute right-4 h-fit w-fit min-w-0 border-none !bg-transparent p-0"
+							on:click={closeSearch}
+						>
+							<iconify-icon icon="heroicons-solid:search" class="text-white/50" />
+						</button>
+						<input
+							tabindex={searchOpen ? 0 : -1}
+							on:blur={closeSearch}
+							class="h-full w-full px-4"
+							type="text"
+							placeholder="Search"
+						/>
+					</div>
+				{/if}
+			</button>
+			<a class="btn h-12" href="##">All posts</a>
+		</div>
+		<div class="pointer-events-none absolute left-0 top-0 h-full w-full">
+			<Noise colour="#1a1a1a" />
+		</div>
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,48 @@
 <script lang="ts">
+	import { browser } from "$app/environment";
 	import ferosBg from "$lib/assets/feros-bg.png";
+	import { onMount } from "svelte";
+
+	let parallax: HTMLImageElement;
+
+	let scroll = browser ? window.scrollY : 0;
+
+	onMount(() => {
+		const anim = (_: number, skipCheck?: boolean) => {
+			if (scroll === window.scrollY && !skipCheck) return requestAnimationFrame(anim);
+			scroll = window.scrollY;
+			parallax.style.transform = `translateY(${scroll * 0.6}px)`;
+			requestAnimationFrame(anim);
+		};
+
+		const handle = requestAnimationFrame(() => anim(0, true));
+		return () => cancelAnimationFrame(handle);
+	});
 </script>
 
 <img
 	aria-hidden
 	src={ferosBg}
+	bind:this={parallax}
 	alt="Background mountains"
-	class="pointer-events-none absolute left-0 top-0 -z-50 h-screen w-screen object-cover"
+	class="pointer-events-none absolute left-0 top-0 -z-50 h-screen w-screen select-none object-cover"
 />
+
+<div class="h-[calc(100vh*2)] w-screen">
+	<div class="flex h-screen w-screen items-center justify-center overflow-hidden px-16 pb-32">
+		<div class="max-w-[1400px]">
+			<h1
+				style="text-shadow: 0 0 40px rgba(255,255,255,0.75); animation: hero-in-right 1.5s cubic-bezier(0.19, 1, 0.22, 1);"
+				class="w-full text-center text-5xl !leading-snug duration-0 lg:text-right lg:text-6xl"
+			>
+				How is the best software in the industry made?
+			</h1>
+			<h2
+				class="mt-16 w-full text-center text-2xl !leading-snug text-white/30 lg:w-1/3 lg:text-left lg:text-3xl"
+				style="animation: hero-in-left 1.5s 0.75s cubic-bezier(0.19, 1, 0.22, 1) forwards; opacity: 0;"
+			>
+				Spoiler alert: it's all talent.
+			</h2>
+		</div>
+	</div>
+</div>
