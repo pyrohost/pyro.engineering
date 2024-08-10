7c19059837c1705606ce230276016116ad2626a0
Null Pointer <nullptr@pyro.host>
2024-08-11 00:53:12 +0100

    feat: no posts msg

--- src/lib/components/landing/PostPreviews.svelte
+++ src/lib/components/landing/PostPreviews.svelte
@@ -12,46 +12,46 @@
 		}[];
 		image: string;
 	}[];
-
-	const formatDate = (date: Date) => {
-		// format the date so its like:
-		// "Sep. 8 2024"
-		// "Jan. 28 2022"
-		const month = date.toLocaleString("default", { month: "short" });
-		const day = date.getDate();
-		const year = date.getFullYear();
-		return `${month}. ${day} ${year}`;
-	};
 </script>
 
 <div class="h-fit w-full">
-	{#each Array.from({ length: Math.ceil(posts.length / 3) }) as _, i (i)}
-		{@const bigIndex = i * 3}
-		{@const leftSmallIndex = bigIndex + 1}
-		{@const rightSmallIndex = bigIndex + 2}
-		<div class="h-full w-full">
-			{#if posts[bigIndex]}
-				{@const post = posts[bigIndex]}
-				<PostPreview half={false} large={true} {post} />
-			{/if}
-			{#if posts[leftSmallIndex] || posts[rightSmallIndex]}
-				{@const leftPost = posts[leftSmallIndex]}
-				{@const rightPost = posts[rightSmallIndex]}
-				<div
-					class={clsx("flex", {
-						// if we're the last element, don't have border-b border-dashed border-neutral-800
-						"border-b border-dashed border-neutral-800":
-							i < Math.ceil(posts.length / 3) - 1,
-					})}
-				>
-					{#if leftPost}
-						<PostPreview half={!!rightPost} large={false} post={leftPost} />
-					{/if}
-					{#if rightPost}
-						<PostPreview half={true} large={false} post={rightPost} />
-					{/if}
-				</div>
-			{/if}
+	{#if posts.length > 0}
+		{#each Array.from({ length: Math.ceil(posts.length / 3) }) as _, i (i)}
+			{@const bigIndex = i * 3}
+			{@const leftSmallIndex = bigIndex + 1}
+			{@const rightSmallIndex = bigIndex + 2}
+			<div class="h-full w-full">
+				{#if posts[bigIndex]}
+					{@const post = posts[bigIndex]}
+					<PostPreview half={false} large={true} {post} />
+				{/if}
+				{#if posts[leftSmallIndex] || posts[rightSmallIndex]}
+					{@const leftPost = posts[leftSmallIndex]}
+					{@const rightPost = posts[rightSmallIndex]}
+					<div
+						class={clsx("flex", {
+							// if we're the last element, don't have border-b border-dashed border-neutral-800
+							"border-b border-dashed border-neutral-800":
+								i < Math.ceil(posts.length / 3) - 1,
+						})}
+					>
+						{#if leftPost}
+							<PostPreview half={!!rightPost} large={false} post={leftPost} />
+						{/if}
+						{#if rightPost}
+							<PostPreview half={true} large={false} post={rightPost} />
+						{/if}
+					</div>
+				{/if}
+			</div>
+		{/each}
+	{:else}
+		<div class="flex h-[50vh] w-full flex-col items-center justify-center gap-3">
+			<iconify-icon
+				icon="heroicons-solid:exclamation-circle"
+				class="text-4xl text-neutral-500"
+			/>
+			<p class="ml-2 font-bold text-neutral-500">...nobody's posted yet. Check back later!</p>
 		</div>
-	{/each}
+	{/if}
 </div>
