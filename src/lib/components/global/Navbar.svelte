a4d7fcb050a92f234da5fd076669d008ab029a07
Null Pointer <nullptr@pyro.host>
2024-08-13 00:32:36 +0100

    fix: darker navbar on firefox

--- src/lib/components/global/Navbar.svelte
+++ src/lib/components/global/Navbar.svelte
@@ -189,7 +189,7 @@
 		<div
 			class="pointer-events-none absolute left-1/2 top-0 -z-10 mt-[1px] h-full w-full max-w-[1400px] -translate-x-1/2 border-b border-dashed border-neutral-800"
 		>
-			<Noise colour={isFirefox ? "#333" : "#666"} />
+			<Noise colour={isFirefox ? "#222" : "#666"} />
 		</div>
 	</div>
 </nav>
