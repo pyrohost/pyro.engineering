eef24b0e2610f96c2b899ae1942ec5e02c289f12
default <theevansong@gmail.com>
2024-08-12 23:38:22 +0000

    fix: hero scaling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,13 +12,13 @@
 
 <div
 	style="background-image: url({ferosBg}); background-size: cover; background-position: center;"
-	class="flex flex-col md:flex-row h-fit w-full items-center border-b border-dashed border-neutral-800 px-8 py-7 text-white [&>*]:leading-normal"
+	class="flex flex-col xl:flex-row h-fit w-full items-center border-b border-dashed border-neutral-800 px-8 py-7 text-white [&>*]:leading-normal"
 >
-	<h1 class="w-full md:max-w-[calc(50%-2px)] flex-shrink-0 flex-grow md:pr-32 text-left text-2xl md:text-4xl font-bold mb-4">
+	<h1 class="w-full xl:max-w-[calc(50%-2px)] flex-shrink-0 flex-grow xl:pr-32 text-left text-2xl xl:text-3xl font-bold mb-4">
 		Pyro develops gaming’s most advanced software.
 	</h1>
-	<div class="hidden md:block h-16 w-0.5 flex-shrink-0 bg-white/30" />
-	<h1 class="w-full md:max-w-[calc(50%-2px)] flex-shrink-0 flex-grow md:pl-32 text-right text-2xl md:text-4xl font-bold">
+	<div class="hidden xl:block h-16 w-0.5 flex-shrink-0 bg-white/30" />
+	<h1 class="w-full xl:max-w-[calc(50%-2px)] flex-shrink-0 flex-grow xl:pl-32 text-right text-2xl xl:text-3xl font-bold">
 		We're makers at heart, and this is our story.
 	</h1>
 </div>
