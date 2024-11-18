58ad282cb7458cd6fb3e6fcb272153834693edf5
Lio <git@lio.cat>
2024-11-18 21:58:15 +0100

    chore: FILTER OUT DRAFTS ON BUILD ON TEAMMEMBER PAGE AND RSS FEED

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,7 +3,9 @@ import type { APIContext } from "astro";
 import { getCollection } from "astro:content";
 
 export async function GET(context: APIContext) {
-  const posts = await getCollection("posts");
+  const posts = await getCollection("posts", ({ data }) => {
+    return import.meta.env.PROD ? data.draft !== true : true;
+  });
   const authors = await getCollection("authors");
   return rss({
     title: "Pyro Engineering",
