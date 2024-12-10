5e1642b56a464ce5c8fd8a0d6ee34a7105970320
Evan Song <theevansong@gmail.com>
2024-12-10 13:07:04 -0700

    refactor!: upgrade astro and integrations to latest
    Signed-off-by: Evan Song <theevansong@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import svelte from '@astrojs/svelte';
-import tailwind from '@astrojs/tailwind';
-import Icons from 'astro-icon';
-import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
-
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import Icons from "astro-icon";
+import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pyro.engineering',
-  integrations: [mdx(), sitemap(), svelte(), tailwind(), react(), Icons({
-    include: {
-      // logos: ['*'],
-      // lucide: ['globe', 'rss']
-    }
-  })],
-  output: 'hybrid',
+  site: "https://pyro.engineering",
+  integrations: [
+    mdx(),
+    sitemap(),
+    svelte(),
+    tailwind(),
+    react(),
+    Icons({
+      include: {
+        // logos: ['*'],
+        // lucide: ['globe', 'rss']
+      },
+    }),
+  ],
 
-  adapter: cloudflare()
+  adapter: cloudflare(),
 });
